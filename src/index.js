e549a5b87f5ecb20f00a8447b7b3e95e6fa6dcab
alitnp <alitnp@umail.com>
2020-10-07 14:10:03 +0330

    solving blank page after build, problem - first try

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./app";
 import "./styles/index.css";
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<App />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById("root")
 );
 
